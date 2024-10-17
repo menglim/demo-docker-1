8929a05419252e15c094aa7cacd506cb91dadb78
Menglim <smaemenglim@gmail.com>
2024-10-17 22:19:05 +0700

    log

--- src/main/java/com/exmaple/demo1/TestController.java
+++ src/main/java/com/exmaple/demo1/TestController.java
@@ -1,15 +1,20 @@
 package com.exmaple.demo1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
+@Slf4j
 @RequestMapping("/api")
 public class TestController {
 
     @RequestMapping("/hello")
     public ResponseEntity<Object> getHello() {
+        log.info("Accessing via Hello " + new Date());
         return ResponseEntity.ok(new Hello("Hello "));
     }
 }
