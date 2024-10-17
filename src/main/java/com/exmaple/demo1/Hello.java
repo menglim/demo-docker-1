ec83e4e3b8dfff6dddb6afacb0ede7f8a337b5a5
Menglim <smaemenglim@gmail.com>
2024-10-17 21:51:35 +0700

    init

---
+++ src/main/java/com/exmaple/demo1/Hello.java
@@ -0,0 +1,12 @@
+package com.exmaple.demo1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hello {
+    private String message;
+}
