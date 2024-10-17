adbc0d134c776555533f5219ee493476baf1f713
Menglim <smaemenglim@gmail.com>
2024-10-17 23:49:42 +0700

    * update

--- src/main/java/com/exmaple/demo1/Demo1Application.java
+++ src/main/java/com/exmaple/demo1/Demo1Application.java
@@ -1,13 +1,26 @@
 package com.exmaple.demo1;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+@Slf4j
+public class Demo1Application implements CommandLineRunner {
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    @Value("${app.version}")
+    private String appVersion;
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Application run successfully. Env " + this.activeProfile + ". Version " + this.appVersion);
+    }
 }
